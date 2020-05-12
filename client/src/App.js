e2eafdd343b03aa521aad42b8ff345994eebd048
devbysalas <rgkalin@gmail.com>
2020-05-11 21:24:38 -0700

    Removes unnecesary stuff from client/ and installs styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>I love musickdkdkdkdkdk</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello world! I love music!</h1>
     </div>
   );
 }
